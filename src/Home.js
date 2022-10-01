4b81aeb3d6e5f84593373af32820bbf6b6cf2a1d
calebthewood <calebwood.cs@gmail.com>
2022-10-01 13:34:23 -0700

    Outline Home component

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+
+
+function Home() {
+
+  return (
+    <div>
+      <div>
+        <h1>
+          Welcome
+        </h1>
+        <h2>I'm Caleb Wood</h2>
+        <h2>Software Enginner & Web Developer</h2>
+      </div>
+      <div>
+        <button>About</button>
+        <button>Portfolio</button>
+        <button>Projects</button>
+      </div>
+    </div>
+  );
+}
