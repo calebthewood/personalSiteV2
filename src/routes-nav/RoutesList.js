4173dd35a7d281c67d39226e1f76d70d70151842
calebthewood <calebwood.cs@gmail.com>
2022-10-09 14:00:51 -0700

    Integrate Tweets

--- src/routes-nav/RoutesList.js
+++ src/routes-nav/RoutesList.js
@@ -10,8 +10,14 @@ import BlogList from "../blog/BlogList";
 
 /**
  * Site-wide Routes
+ *
+ * When I have more posts, I'll distribute them to the other routes. So that
+ * relevant blogs can be shown alongside projects. Or at the least, linked to.
  */
-export default function RoutesList() {
+export default function RoutesList({ isLoading, allPosts, tweets }) {
+  // Routes to add later. to view just a post or just a project, no menus/header
+  //     {/* <Route path="/projects/:project" element={<ProjectDetail />} /> */}
+  //     {/* <Route path="/blog/:id" element={<BlogPost />} /> */}
   return (
     <div>
       <Routes>
@@ -20,11 +26,11 @@ export default function RoutesList() {
         <Route path="/portfolio" element={<PortfolioList />} />
         <Route path="/projects" element={<ProjectList />} />
         <Route path="/projects/:param" element={<ProjectList />} />
-        {/* <Route path="/projects/:project" element={<ProjectDetail />} /> */}
-        <Route path="/blog/posts/:showing" element={<BlogList />} />
-        {/* <Route path="/blog/:id" element={<BlogPost />} /> */}
-
-        <Route path="*" element={<Navigate to="/" />}/>
+        <Route path="/blog/posts/:showing" element={<BlogList
+                                                      isLoading={isLoading}
+                                                      allPosts={allPosts}
+                                                      tweets={tweets} />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
