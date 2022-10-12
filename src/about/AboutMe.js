0b09bba28d1dd60d1ce52c4d5f4e64719b7214ca
calebthewood <calebwood.cs@gmail.com>
2022-10-12 13:39:02 -0700

    About section CSS changes

--- src/about/AboutMe.js
+++ src/about/AboutMe.js
@@ -5,19 +5,19 @@ import "./aboutMe.css";
 export default function AboutMe() {
   const sectionData = [{
     url: "/portfolio",
-    title: "Portfolio",
+    title: "View Portfolio",
     subtitle: "Click to see my portfolio of fullstack applications",
     description: "maybe more"
   },
   {
     url: "/projects",
-    title: "Projects",
+    title: "View Projects",
     subtitle: "Click to see a collection of projects and components",
     description: "maybe more"
   },
   {
     url: "/blog-posts",
-    title: "Blog",
+    title: "View Blog",
     subtitle: "Click to see my articles on development, and more",
     description: "maybe more"
   },
@@ -27,7 +27,7 @@ export default function AboutMe() {
       <div className="row d-flex justify-content-evenly">
         <div className="col-xs-12 col-md-4 col-lg-4">
           <div
-            className="card bg-transparent about-card my-3">
+            className="card text-bg-dark about-card my-3 shadow">
             <img className="card-img-top" src="https://s3.us-west-1.amazonaws.com/pix.ly/selfie.jpg" alt="Caleb's Selfie" />
             <div className="card-body text-center">
               <div><i className="fas fa-map-marker-alt"></i> San Francisco, CA</div>
@@ -45,25 +45,26 @@ export default function AboutMe() {
 
         <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
           <div
-            className="card bg-transparent about-card my-3 p-4">
-            <h5 className="card-title">A little about myself</h5>
+            className="card text-bg-dark my-3 p-4">
+            <h5 className="card-title">A little about myself, and the site</h5>
             <ul className="list-group list-group-flush">
               <br />
-              <li className="list-group-item bg-transparent">
-                I am a software engineer with an emphasis on fullstack web development. The whole stack,
-                you ask? Yes, the whole stack! I enjoy writing a clever SQL query almost as much as I
-                like building a clean, interactive UI, and everything in between too. Most recently,
-                I've spent a lot of time working in the Python Flask ecosystem, but I'm just as comfortable
-                building in Node. See <a href="/public/Caleb_Wood_Resume.pdf">resume</a> for
-                professional and personal experience.
-              </li>
-              <li className="list-group-item bg-transparent">
-                When I'm not coding up <a href="#projects">programs</a> for mundane problems I like
-                getting out in nature, baking my way towards the perfect baguette,
-                and, as an avid learner, of course, reading. Fiction, non-fiction,
-                blogs, articles, docs I read it all!
-              </li>
+              <li className="list-group-item text-bg-dark">
+                I'm a fullstack web developer and software engineer. This site is
+                my current workshop. You should see new features and projects being
+                added each week, along with blog posts to walk through implementation
+                details. I've had a lot of fun building it, I hope you enjoy it!
 
+              </li>
+              <li className="list-group-item text-bg-dark">
+                When I'm not coding up programs for mundane problems I like
+                biking around the city, baking my way towards the perfect baguette
+                and, as an avid learner, of course, reading.
+              </li>
+              <li className="list-group-item text-bg-dark">
+              See <a className="plain-link" href="/public/Caleb_Wood_Resume.pdf">resume</a> for
+                professional experience.
+                </li>
             </ul>
           </div>
         </div>
