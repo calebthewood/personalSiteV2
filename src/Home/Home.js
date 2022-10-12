994fc47c76db81f087dd0932b8675a3c4e35f8ee
calebthewood <calebwood.cs@gmail.com>
2022-10-12 13:39:50 -0700

    Home CSS changes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
+import "./home.css";
 
 export default function Home() {
 
   return (
-    <div>
-      <div>
+    <div id="home-page" className="row align-items-center">
+      <div className="col">
         <h1>
           Welcome
         </h1>
         <h2>I'm Caleb Wood</h2>
         <h2>Software Enginner and Web Developer</h2>
       </div>
-      <div>
-        <Link to="/about"><button>About</button></Link>
-        <Link to="/portfolio"><button>Portfolio</button></Link>
-        <Link to="/projects"><button>Projects</button></Link>
-        <Link to="/blog/posts/recent"><button>Blog</button></Link>
+      <div id="home-links" className="row">
+        <div id="home-links" className="col">
+          <Link to="/about"><button>About</button></Link>
+          <Link to="/portfolio"><button>Portfolio</button></Link>
+          <Link to="/projects"><button>Projects</button></Link>
+          <Link to="/blog/posts/recent"><button>Blog</button></Link>
+        </div>
       </div>
     </div>
   );
