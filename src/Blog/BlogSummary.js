b0594e01c8f46c54249502e9b505ed8bbe299b68
calebthewood <calebwood.cs@gmail.com>
2022-10-12 20:58:26 -0700

    Change bg color

--- src/Blog/BlogSummary.js
+++ src/Blog/BlogSummary.js
@@ -6,18 +6,22 @@ export default function BlogSummary({ post }) {
   // const description = post.blocks[1].paragraph.plain_text[0].slice(0,50)
 
   return (
-    <div className="card text-bg-dark mb-3">
-      <div className="card-body">
-        <Link className="navigation-link" to={`/blog/posts/${post.slug}`}>
-          <h5 className="card-title">{post.title}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{post.author} - {post.date}</h6>
-          <p className="card-text">Filler description, will either add a field to the blog, or find a nice way to show the first x chars from content</p>
-        </Link>
-        {post.tags.map(tag => <Link
-                                key={tag.id}
-                                to={`/blog/posts/${tag.name}`}
-                                className="card-link"> {tag.name} </Link>)}
+    <li className="list-group-item text-bg-dark">
+      <div className="card blog-summary text-bg-dark">
+        <div className="card-body">
+          <Link className="navigation-link" to={`/blog/posts/${post.slug}`}>
+            <h5 className="card-title">{post.title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{post.author} - {post.date}</h6>
+            <p className="card-text mb-3">Filler description, will either add a field to the blog, or find a nice way to show the first x chars from content</p>
+          </Link>
+
+          {post.tags.map(tag => <Link
+            key={tag.id}
+            to={`/blog/posts/${tag.name}`}
+            className={`tags ${tag.color} mx-1`}> {tag.name} </Link>)}
+
+        </div>
       </div>
-    </div>
+    </li>
   );
 }
