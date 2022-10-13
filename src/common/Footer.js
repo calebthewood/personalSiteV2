50db22676a0e314561c5ec29ddfb164a2eb0609b
calebthewood <calebwood.cs@gmail.com>
2022-10-13 12:53:23 -0700

    Finalized footer design

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -4,34 +4,28 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   const { pathname } = useLocation();
 
-  if (pathname) return <></>;
+  if (pathname === "/") return <></>;
 
   return (
-    <footer className="py-3 text-bg-dark text-center container-flex">
+    <footer className="py-3 mt-5 text-bg-dark text-center container-flex">
       <div className="container pt-2">
-        <h5>Contact Me by email with any questions:</h5>
-        <p><a class="contactLinks" href="mailto:calebwood.cs@gmail.com">calebwood.cs@gmail.com</a></p>
+        <h5>Contact Me by email with any questions</h5>
+        <p><a type="button" className="email-btn btn btn-dark" href="mailto:calebwood.cs@gmail.com">calebwood.cs@gmail.com</a></p>
         <hr className="border border-light" />
-        <ul className="nav justify-content-center my-3">
 
-          <li>
-            <a className="contact-icon" href="mailto:calebwood.cs@gmail.com">
-              <i className="fa-solid fa-envelope"></i>
+        <ul id="about-btns" className="nav justify-content-center my-3">
+          <li className="list-group-item btn-group text-bg-dark">
+            <a type="button" className="btn btn-dark" href="mailto:calebwood.cs@gmail.com">
+              <i className="about-icons fa-solid fa-envelope"></i>
             </a>
-          </li>
-          <li>
-            <a className="contact-icon" href="https://www.linkedin.com/in/caleb-wood-440b37168/">
-              <i className="fa-brands fa-linkedin"></i>
+            <a type="button" className="btn btn-dark" href="https://www.linkedin.com/in/caleb-wood-440b37168/">
+              <i className="about-icons fa-brands fa-linkedin"></i>
             </a>
-          </li>
-          <li>
-            <a className="contact-icon" href="https://github.com/calebthewood">
-              <i className="fa-brands fa-github"></i>
+            <a type="button" className="btn btn-dark" href="https://github.com/calebthewood">
+              <i className="about-icons fa-brands fa-github"></i>
             </a>
-          </li>
-          <li>
-            <a className="contact-icon" href="https://www.instagram.com/calebthewood/?hl=en">
-              <i className="fa-brands fa-instagram"></i>
+            <a type="button" className="btn btn-dark" href="https://www.instagram.com/calebthewood/?hl=en">
+              <i className="about-icons fa-brands fa-instagram"></i>
             </a>
           </li>
         </ul>
