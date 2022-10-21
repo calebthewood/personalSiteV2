bb528643819969623fc7b003fcd4ee6c502f0afa
Caleb <84254057+calebthewood@users.noreply.github.com>
2022-10-21 09:25:56 -0700

    Delete src/twitterApi directory

--- src/twitterApi/twitterAPI.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export class TwitterAPI {
-  static async getTweetsByTag(tag) {
-    const url = `https://project-pith.herokuapp.com/tweets/${tag}`;
-    let { data } = await axios.get(url);
-    return data.response;
-  }
-}
