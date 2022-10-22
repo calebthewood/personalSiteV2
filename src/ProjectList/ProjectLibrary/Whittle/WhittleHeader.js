db7d5a8497619c08eebfcc025767c3879bdd806e
calebthewood <calebwood.cs@gmail.com>
2022-10-21 22:28:23 -0700

    WhittleHeader update

--- src/ProjectList/ProjectLibrary/Whittle/WhittleHeader.js
+++ src/ProjectList/ProjectLibrary/Whittle/WhittleHeader.js
@@ -8,7 +8,8 @@ export default function WhittleHeader() {
       <div className="col">
           <p>Welcome to <i>whittle</i>, my wordle-assist app. Enter your wordle
           guesses, and then click or press enter to change the color, and whittle
-          down the number of potential words to choose from. </p>
+          down the number of potential words to choose from. Currently this app
+          does function well on mobile devices.</p>
         </div>
       </div>
 
