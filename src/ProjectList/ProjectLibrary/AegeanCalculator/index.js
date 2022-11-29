57aa369511e9fba22c1baa4132611dabc7ea141c
calebthewood <calebwood.cs@gmail.com>
2022-11-28 21:17:00 -0800

    Add styles, handleClick WIP

--- src/ProjectList/ProjectLibrary/AegeanCalculator/index.js
+++ src/ProjectList/ProjectLibrary/AegeanCalculator/index.js
@@ -1,41 +1,66 @@
 import "./AegeanCalculator.css";
-import { memo, useState } from "react";
-import { createMemoryHistory } from "@remix-run/router";
+import { useState } from "react";
+
 
 export function AegeanCalculator() {
   const initMemory = {
     currentVal: "",
-    nextVal: false,
-    operator: false,
+    nextVal: "",
+    operator: "",
   };
   const [memory, setMemory] = useState(initMemory);
   const [display, setDisplay] = useState(memory.currentVal);
 
   function handleClick(evt) {
     const { value } = evt.target;
-    const { currentVal, nextVal, operator } = memory;
+    let { currentVal, nextVal, operator } = memory;
+    let newDisplay;
 
     if (value === "ac") {
-      setMemory(initMemory);
-      setDisplay(memory.currentVal);
-    } else if (value === "=") {
+      currentVal = "";
+      nextVal = "";
+      operator = "";
+    } else if (operator && value === "=") {
       if (operator === "-") {
-
-      } else if (operator === "+") { }
-        else if (operator === "/") { }
-        else if (operator === "x") { }
-
+        currentVal = Number(currentVal) - Number(nextVal);
+      } else if (operator === "+") {
+        currentVal = Number(currentVal) + Number(nextVal);
+      } else if (operator === "/") {
+        currentVal = Number(currentVal) / Number(nextVal);
+      } else if (operator === "x") {
+        currentVal = Number(currentVal) * Number(nextVal);
+      }
+      newDisplay = currentVal;
+      operator = ""
+    } else if ("0123456789".includes(value)) {
+      if (operator) {
+        nextVal += value;
+        newDisplay = `${currentVal} ${operator} ${nextVal}`
+        operator = "";
+      } else {
+        currentVal += value;
+        newDisplay = currentVal;
+      }
+    } else {
+      operator = value;
+      newDisplay = `${currentVal} ${operator}`;
     }
 
+    setMemory({
+      currentVal: String(currentVal),
+      nextVal: String(nextVal),
+      operator: String(operator),
+    });
+    setDisplay(newDisplay);
   }
 
 
   return (
     <div className="calculator">
-      <div className="display">
-        <span >{display}</span>
+      <div className="row display">
+        {display}
       </div>
-      <div className="buttons">
+      <div onClick={handleClick} className="buttons">
         <div className="row calc-btns">
           <button
             type="button"
@@ -45,59 +70,79 @@ export function AegeanCalculator() {
             value="+/-">+/-</button>
           <button
             type="button"
-            value="%">%</button>
+            value="%" disabled>%</button>
           <button
             type="button"
-            value="/">/</button>
+            value="/"
+            className="rounded-0 btn btn-warning">/</button>
         </div>
         <div className="row calc-btns">
           <button
             type="button"
-            value="7">7</button>
+            value="7"
+            className="rounded-0 btn btn-light">7</button>
           <button
             type="button"
-            value="8">8</button>
+            value="8"
+            className="rounded-0 btn btn-light">8</button>
           <button
             type="button"
-            value="9">9</button>
+            value="9"
+            className="rounded-0 btn btn-light">9</button>
           <button
             type="button"
-            value="x">x</button>
+            value="x"
+            className="rounded-0 btn btn-warning">x</button>
         </div>
         <div className="row calc-btns">
           <button
             type="button"
-            value="4">4</button>
+            value="4"
+            className="rounded-0 btn btn-light">4</button>
           <button
             type="button"
-            value="5">5</button>
+            value="5"
+            className="rounded-0 btn btn-light">5</button>
           <button
             type="button"
-            value="6">6</button>
+            value="6"
+            className="rounded-0 btn btn-light">6</button>
           <button
             type="button"
-            value="-">-</button>
+            value="-"
+            className="rounded-0 btn btn-warning">-</button>
         </div>
         <div className="row calc-btns">
           <button
             type="button"
-            value="1">1</button>
+            value="1"
+            className="rounded-0 btn btn-light">1</button>
           <button
             type="button"
-            value="2">2</button>
+            value="2"
+            className="rounded-0 btn btn-light">2</button>
           <button
             type="button"
-            value="3">3</button>
+            value="3"
+            className="rounded-0 btn btn-light">3</button>
           <button
-            type="button">+</button>
+            type="button"
+            className="rounded-0 btn btn-warning">+</button>
         </div>
         <div className="row calc-btns">
           <button
             id="calc-0"
             type="button"
-            value="0"></button>
-          <button>.</button>
-          <button>=</button>
+            value="0"
+            className="rounded-0 btn btn-light">0</button>
+          <button
+            type="button"
+            value="."
+            className="rounded-0 btn btn-light" disabled>.</button>
+          <button
+            type="button"
+            value="+"
+            className="rounded-0 btn btn-warning">=</button>
         </div>
       </div>
     </div>
