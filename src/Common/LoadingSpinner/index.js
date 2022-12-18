df415ed91aa70783fcda7c8a3d72392daf970359
calebthewood <calebwood.cs@gmail.com>
2022-12-18 11:09:17 -0800

    Improve Loading Spinner

--- src/Common/LoadingSpinner/index.js
+++ src/Common/LoadingSpinner/index.js
@@ -1,9 +1,11 @@
-import "./LoadingSpinner.css"
+import "./LoadingSpinner.css";
 export function LoadingSpinner() {
 
   return (
-    <div className="spinner-container">
-      <div className="loading-spinner">
+    <div className="row justify-content-center align-items-center spinner-container">
+      <div className="col-2 loading-spinner"></div>
+      <div className="col-3 mx-2">
+        <span className="h1">Loading...</span>
       </div>
     </div>
   );
