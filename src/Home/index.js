d414ab5c5b80371835c53d25690ee6943b9d2603
calebthewood <calebwood.cs@gmail.com>
2023-03-31 11:57:14 -0700

    Styling home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -5,60 +5,49 @@ import { BoxCanvas } from "./BoxCanvas";
 export function Home() {
 
   return (
-    <div id="home-container" className="container col mb-4">
+    <div id="home-container" className="wrapper">
 
-      <div id="home-row" className="row justify-content-between align-items-end mb-5">
+      <div id="home-row" className="hero">
 
-        <div className="col-12 col-md-6 mt-5">
+        <div className="">
           <h1>
             Welcome
           </h1>
           <h2>I'm Caleb Wood,</h2>
           <h2>Full Stack Web Developer</h2>
         </div>
-        <div className="col" id="home-animation">
+        <div className="" id="home-animation">
           <BoxCanvas />
         </div>
       </div>
 
-      <div className="row justify-content-around gy-4">
-        <div className="col-sm-2 col-5">
-          <Link to="/about">
+          <Link className="about" to="/about">
             <button
               id="about-btn"
               type="button"
-              className="btn btn-lg btn-outline-dark">About</button>
+              className="home-btns">About</button>
           </Link>
-        </div>
 
-        <div className="col-sm-2 col-5 mx-2">
-          <Link to="/portfolio">
+          <Link className="portfolio" to="/portfolio">
             <button
               id="portfolio-btn"
               type="button"
-              className="btn btn-lg btn-outline-dark">Portfolio</button>
+              className="home-btns">Portfolio</button>
           </Link>
-        </div>
 
-        <div className="col-sm-2 col-5 mx-2">
-          <Link to="/projects">
+          <Link className="projects" to="/projects">
             <button
               id="projects-btn"
               type="button"
-              className="btn btn-lg btn-outline-dark">Projects</button>
+              className="home-btns">Projects</button>
           </Link>
-        </div>
 
-        <div className="col-sm-2 col-5 mx-2">
-          <Link to="/blog/posts/recent">
+          <Link className="blog" to="/blog/posts/recent">
             <button
               id="blog-btn"
               type="button"
-              className="btn btn-lg btn-outline-dark">Blog</button>
+              className="home-btns">Blog</button>
           </Link>
-        </div>
-
-      </div>
     </div>
   );
 }
